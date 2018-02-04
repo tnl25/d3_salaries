e4f4abc2f66a132b980c2cd1ea97b42fcc2fd09f
tnl25 <tnl25@cornell.edu>
2018-02-04 12:31:56 -0800

    update readme and index js

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ STEP 1: Install http-server
 
 Run `npm install http-server -g` to install http-server globally.
 
-Read more about http-server for node from https://github.com/indexzero/http-server
+Read more about http-server for node from https://www.npmjs.com/package/http-server
 
 STEP 2: Run the server
 
-Once http-server is set up, you can run `http-server -c-1` to set up the server and `-c-1` is an option to disable caching since caching is on by default.
+Once http-server is set up, you can run `http-server p 3000 -c-1` to set up the server and `-c-1` is an option to disable caching since caching is on by default.
 
-Run `localhost:8080`
+Run `localhost:3000`
--- index.js
+++ index.js
@@ -1,119 +1,109 @@
 /* eslint-disable */
 d3.csv('salaries.csv', function(data){
-    // trimmed = function(data) {
-    //     ret = []
-    //     data.forEach(element => {
-    //         if (element.BasePay > 10000 && element.TotalPay > 10000) {
-    //             ret.push(element)
-    //         }
-    //     });
-    //     return ret
-    // }(data)
-    // data = trimmed
 	var body = d3.select('body')
 	var margin = {top:50,right:50,left:50,bottom:50}
 	var h = 700 - margin.top - margin.bottom
-    var w  = 700 - margin.left - margin.right
-    // svg  
+	var w  = 700 - margin.left - margin.right
+	// svg  
 	var svg = body.append('svg')
-    .attr('height', h + margin.top + margin.bottom)
-    .attr('width', w + margin.left + margin.right)
-    .data(data)
-    .enter()
-    .append('g')
-        .attr('transform','translate(' + margin.left + ',' + margin.top + ')')
-    // scales
-    var colorScale = d3.scaleOrdinal(d3.schemeCategory20);
-    var xScale = d3.scaleLinear()
-        // set domain to a specific number because the BasePay field has string values
+		.attr('height', h + margin.top + margin.bottom)
+		.attr('width', w + margin.left + margin.right)
+		.append('g')
+		.attr('transform','translate(' + margin.left + ',' + margin.top + ')')
+	// scales
+	var colorScale = d3.scaleOrdinal(d3.schemeCategory20)
+	var xScale = d3.scaleLinear()
+		// set domain to a specific number because the BasePay field has string values
 		.domain([50000,350000])
-        .rangeRound([0,w])
-    var yScale = d3.scaleLinear()
-        // set domain to a specific number because the number exists outside of scale
+		.rangeRound([0,w])
+	var yScale = d3.scaleLinear()
+		// set domain to a specific number because the number exists outside of scale
 		.domain([50000,480000])
-        .rangeRound([h,0])
-    // X-axis
+		.rangeRound([h,0])
+	// X-axis
 	var xAxis = d3.axisBottom()
-        .scale(xScale)
-        .ticks(5)
-    // Y-axis
-    var yAxis = d3.axisLeft()
-        .scale(yScale)
-        .ticks(5)
+		.scale(xScale)
+		.ticks(5)
+	// Y-axis
+	var yAxis = d3.axisLeft()
+		.scale(yScale)
+		.ticks(5)
 
-    var circles = svg.selectAll('circle')
-        .data(data)
-        .enter()
-        .append('circle')
-            .filter(function(d){return d.Year = 2014 && d.BasePay > 50000 && d.TotalPay > 50000})
-            .attr('cx',function (d) { 
-                // v = xScale(d.BasePay); 
-                // console.log("Total Pay is "+ v + " and " + d.BasePay); 
-                return xScale(d.BasePay)
-            })
-            .attr('cy',function (d) { 
-                // v = yScale(d.TotalPay); 
-                // console.log("Total Pay is "+ v + " and " + d.TotalPay); 
-                return yScale(d.TotalPay) 
-            })
-            .attr('r','5')
-            .attr('stroke','black')
-            .attr('stroke-width',1)
-            .attr('fill',function (d,i) { return colorScale(i) })
-            .on('click', function () {
-                d3.select(this)
-                    .transition()
-                    .duration(500)
-                    .attr('r',10)
-                    .attr('stroke-width',2)
-                    // .append('text')
-                    //     .text(function(d){
-                    //         str = "Base: $" +  d.BasePay + "Total: $" + d.TotalPay
-                    //         // console.log(str)
-                    //         return str
-                    //     })
-                    //     .style("text-anchor","middle")
-                })
-            .on('mouseout', function () {
-                d3.select(this)
-                    .transition()
-                    .duration(500)
-                    .attr('r',5)
-                    .attr('stroke-width',1)
-            })
-// below append MULTIPLE text on top of each other
-    // var texts = svg.selectAll('p')
-    //     .append('text')
-    //     .text(function(d){
-    //         str = "BasePay: " +  d.BasePay 
-    //         // console.log(str)
-    //         return str
-    //     })
+	var circles = svg.selectAll('circle')
+		.data(data)
+		.enter()
+		.append('circle')
+		.filter(function(d){return d.Year = 2014 && d.BasePay > 50000 && d.TotalPay > 50000})
+		.attr('cx',function (d) { 
+			// v = xScale(d.BasePay); 
+			// console.log("Total Pay is "+ v + " and " + d.BasePay); 
+			return xScale(d.BasePay)
+		})
+		.attr('cy',function (d) { 
+			// v = yScale(d.TotalPay); 
+			// console.log("Total Pay is "+ v + " and " + d.TotalPay); 
+			return yScale(d.TotalPay) 
+        })
+		.attr('r','5')
+		.attr('stroke','black')
+		.attr('stroke-width',1)
+        .attr('fill',function (d,i) { return colorScale(i) })
+        .attr('class','circle')
+		.on('click', function () {
+			d3.select(this)
+				.transition()
+				.duration(500)
+				.attr('r',10)
+				.attr('stroke-width',2)
+				// .append('text')
+			    // .text(function(d){
+			    //     str = 'Base: $' +  d.BasePay + 'Total: $' + d.TotalPay
+			    //     // console.log(str)
+			    //     return str
+			    // })
+			    // .style('text-anchor','middle')
+		})
+		.on('mouseout', function () {
+			d3.select(this)
+				.transition()
+				.duration(500)
+				.attr('r',5)
+				.attr('stroke-width',1)
+		})
+	// below append MULTIPLE text on top of each other
+	// var texts = svg.selectAll('p')
+	//     .append('text')
+	//     .text(function(d){
+	//         str = "BasePay: " +  d.BasePay 
+	//         // console.log(str)
+	//         return str
+	//     })
 
-    // X-axis
+	// X-axis
     svg.append('g')
-        .attr('class','axis')
-        .attr('transform', 'translate(0,' + h + ')')
-        .call(xAxis)
-        .append('text') // X-axis Label
+		.attr('class','axis')
+		.attr('transform', 'translate(0,' + h + ')')
+		.call(xAxis)
+		.append('text') // X-axis Label
         .attr('class','label')
-        .attr('y',-10)
-        .attr('x',w)
-        .attr('dy','.71em')
-        .style('text-anchor','end')
         .text('Base Pay')
-    // Y-axis
-    svg.append('g')
-        .attr('class', 'axis')
-        .call(yAxis)
-        .append('text') // y-axis Label
-        .attr('class','label')
-        .attr('transform','rotate(-90)')
-        .attr('x',0)
-        .attr('y',5)
-        .attr('dy','.71em')
-        .style('text-anchor','end')
-        .text('Total Pay')
+		.attr('y',-50)
+		.attr('x',w)
+		.attr('dy','.71em')
+		.style('text-anchor','end')
+		
+	// Y-axis
+	svg.append('g')
+		.attr('class', 'axis')
+		.call(yAxis)
+		.append('text') // y-axis Label
+		.attr('class','label')
+		.attr('transform','rotate(-90)')
+		.attr('x',0)
+		.attr('y',5)
+		.attr('dy','.71em')
+		.style('text-anchor','end')
+		.text('Total Pay')
 
 })
 
